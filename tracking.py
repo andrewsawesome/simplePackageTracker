5d4e67931f49efc203877e5cb78f348f429d3521
andrewsawesome <36270171+andrewsawesome@users.noreply.github.com>
2025-03-30 21:11:37 -0400

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -13,6 +13,16 @@ match carrier:
         trackingLink = ("https://www.ups.com/track?sort_by=status&tracknums_displayed=1&TypeOfInquiryNumber=T&loc=en_us&InquiryNumber1=" + trackingNumber + U"&requester=ST")
     case "FedEx" | "Fedex" | "fedex":
         trackingLink = ("https://www.fedex.com/wtrk/track/?trknbr=" + trackingNumber)
+    case "dhl" | "DHL":
+        trackingLink = ("https://www.dhl.com/us-en/home/tracking.html?tracking-id=" + trackingNumber)
+    case "canada post" | "Canada Post":
+        trackingLink = ("https://www.canadapost-postescanada.ca/track-reperage/en#/search?searchFor=" + trackingNumber)
+    case "royal mail" | "Royal Mail":
+        trackingLink = ("https://www.royalmail.com/track-your-item#/tracking-results/" + trackingNumber)
+    case "YunExpress" | "yunexpress" | "yun express":
+        trackingLink = ("https://www.yuntrack.com/parcelTracking?id=" + trackingNumber)
+    case "4PX" | "4px" | "4 px":
+        trackingLink = ("https://m-track.4px.com/#/details/" + trackingNumber)
 #Open tracking link/display error
 if trackingLink == 0:
     print("Your package carrier may be incorrectly formatted or not supported")
