acd61976bec5ccb023f7a6c2c6305496286a5022
andrewsawesome <36270171+andrewsawesome@users.noreply.github.com>
2025-06-10 16:24:59 -0400

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -13,16 +13,20 @@ match carrier:
         trackingLink = ("https://www.ups.com/track?sort_by=status&tracknums_displayed=1&TypeOfInquiryNumber=T&loc=en_us&InquiryNumber1=" + trackingNumber + U"&requester=ST")
     case "FedEx" | "Fedex" | "fedex":
         trackingLink = ("https://www.fedex.com/wtrk/track/?trknbr=" + trackingNumber)
-    case "dhl" | "DHL":
+    case "DHL" | "dhl":
         trackingLink = ("https://www.dhl.com/us-en/home/tracking.html?tracking-id=" + trackingNumber)
-    case "canada post" | "Canada Post":
+    case "Australia Post" | "australia post":
+        trackingLink = ("https://auspost.com.au/mypost/track/details/" + trackingNumber)
+    case "Canada Post" | "canada post":
         trackingLink = ("https://www.canadapost-postescanada.ca/track-reperage/en#/search?searchFor=" + trackingNumber)
-    case "royal mail" | "Royal Mail":
+    case "New Zealand Post" | "new zealand post":
+        trackingLink = ("https://www.nzpost.co.nz/tools/tracking?trackid=" + trackingNumber)
+    case "Royal Mail" | "royal mail":
         trackingLink = ("https://www.royalmail.com/track-your-item#/tracking-results/" + trackingNumber)
+    case "4PX" | "4px" | "4 px":
+        trackingLink = ("https://m-track.4px.com/#/details/" + trackingNumber)    
     case "YunExpress" | "yunexpress" | "yun express":
         trackingLink = ("https://www.yuntrack.com/parcelTracking?id=" + trackingNumber)
-    case "4PX" | "4px" | "4 px":
-        trackingLink = ("https://m-track.4px.com/#/details/" + trackingNumber)
 #Open tracking link/display error
 if trackingLink == 0:
     print("Your package carrier may be incorrectly formatted or not supported")
